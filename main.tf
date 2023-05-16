3f3a9e268b618ed254110c15d5cefb7d427c0108
Douglas Francis <douglas@poshops.io>
2023-05-16 17:13:14 -0500

    checkov testing

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_log_analytics_solution" "sentinel" {
 #   log_analytics_workspace_id = data.terraform_remote_state.core.outputs.core_la_workspace_id
 # }
 
-
+# not needed for most clients
 # resource "azurerm_sentinel_data_connector_threat_intelligence" "sentinel-taxii" {
 #   name                       = "TAXII"
 #   log_analytics_workspace_id = data.terraform_remote_state.core.outputs.core_la_workspace_id
