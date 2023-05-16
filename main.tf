a1ad582cac28f21e659772e14aa5737189dac152
Douglas Francis <douglas@poshops.io>
2023-05-16 17:16:41 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,8 @@ resource "azurerm_log_analytics_solution" "sentinel" {
 #   log_analytics_workspace_id = data.terraform_remote_state.core.outputs.core_la_workspace_id
 # }
 
-# not needed for most clients
+# not needed for most clients, additional steps to configure if desired.
+
 # resource "azurerm_sentinel_data_connector_threat_intelligence" "sentinel-taxii" {
 #   name                       = "TAXII"
 #   log_analytics_workspace_id = data.terraform_remote_state.core.outputs.core_la_workspace_id
