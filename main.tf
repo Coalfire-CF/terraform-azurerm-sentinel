54ff4a4ccb2e7ec4c4af902d39668406e982ed3d
Matthew Pappas <matthew.pappas@coalfire.com>
2025-12-12 11:05:07 -0800

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_sentinel_log_analytics_workspace_onboarding" "example" {
+resource "azurerm_sentinel_log_analytics_workspace_onboarding" "this" {
   workspace_id                 = var.log_analytics_workspace_id
   customer_managed_key_enabled = var.customer_managed_key_enabled
 }
